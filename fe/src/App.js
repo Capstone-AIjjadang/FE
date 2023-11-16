21cfdc6257af8a5d62245b52432d7f787eaf831d
cj <toong1203@naver.com>
2023-11-16 16:15:49 +0900

    [feat]음식분석 페이지
    01. 주소창 : /camera/analysis 로 매핑
    02. 목 데이터 못 넣음

--- fe/src/App.js
+++ fe/src/App.js
@@ -3,6 +3,7 @@ import Navigator from './components/Navigator';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import FoodRecommend from './components/FoodRecommend';
 import Main from './components/Main';
+import CameraAnalysis from './components/CameraAnalysis';
 import './App.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/camera" element={<Camera />} />
         <Route path="/recommend" element={<FoodRecommend />} />
+        <Route path="/camera/analysis" element={<CameraAnalysis />} />
       </Routes>
       {/* <Navigator /> */}
       {HideNavigator ? '' : <Navigator />}
---
+++ fe/src/components/CameraAnalysis.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import style from "../css/CameraAnalysis.module.css";
+import Navigator from './Navigator';
+import { LinearProgress } from '@mui/material';
+
+const CameraAnalysis = () => {
+    const [rating, setRating] = React.useState(0);
+    const handleRatingChange = (value) => {
+        setRating(value);
+    };
+
+    return (
+        <div className={style.container}>
+            <div className={style.header}>
+                <img src={"/imgs/Logo.png"} height="50px" />
+            </div>
+            <form className={style.form}>
+                <div className={style.screen_container}>
+                    <div><img src={"/imgs/foodsample.jpg"} height="300" /></div>
+                </div>
+                <div className={style.analysis_container}>
+                    <div className={style.foodname}>인식된 음식이름</div>
+                    <div className={style.analysis}>
+                        <div className={style.sec1}>
+                            <li>총 칼로리 <span>00Kcal</span>
+                                <LinearProgress variant="determinate" value={70} style={{ width: '100px', height: '14px', borderRadius: '10px' }} />
+                            </li>
+                            <br /><br /><br />
+                            <li>탄수화물<span>00g</span>
+                                <LinearProgress variant="determinate" value={50} style={{ width: '100px', height: '14px', borderRadius: '10px' }} />
+                            </li>
+                        </div>
+                        <div className={style.sec2}>
+                            <li>나트륨<span>00g</span>
+                                <LinearProgress variant="determinate" value={100} style={{ width: '100px', height: '14px', borderRadius: '10px' }} />
+                            </li>
+                            <br /><br /><br />
+                            <li>단백질<span>00g</span>
+                                <LinearProgress variant="determinate" value={30} style={{ width: '100px', height: '14px', borderRadius: '10px' }} />
+                            </li>
+                        </div>
+                        <div className={style.sec3}>
+                            <li>지방<span>00g</span>
+                                <LinearProgress variant="determinate" value={50} style={{ width: '100px', height: '14px', borderRadius: '10px' }} />
+                            </li>
+                            <br /><br /><br />
+                            <li>칼슘<span>00g</span>
+                                <LinearProgress variant="determinate" value={50} style={{ width: '100px', height: '14px', borderRadius: '10px' }} />
+                            </li>
+                        </div>
+                    </div>
+                </div>
+                <div className={style.info_container}>
+                    <div className={style.title}>
+                        얼마나 드셨나요?
+                    </div>
+                    <div className={style.gagebar}>
+                        {[1, 2, 3, 4].map((space) => (
+                            <div
+                                key={space}
+                                onClick={() => handleRatingChange(space / 4)}
+                                style={{
+                                    display: 'flex',
+                                    flex: 1,
+                                    cursor: 'pointer',
+                                    textAlign: 'center',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    backgroundColor: space / 4 <= rating ? '#6C95EC' : '#F5EBEB',
+                                    marginLeft: '3px',
+                                }}
+                            >
+                                {space}/{4}
+                            </div>
+                        ))}
+                        {rating}
+                    </div>
+                </div>
+                <div className={style.btn_container}>
+                    <button className={style.btn_record}>기록하기</button>
+                </div>
+            </form>
+            <Navigator />
+        </div>
+    );
+};
+
+export default CameraAnalysis;
