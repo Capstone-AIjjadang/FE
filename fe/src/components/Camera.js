1aaf2eca0529733f3c78a8e5fc81014209655fa2
cj <toong1203@naver.com>
2023-11-23 23:28:55 +0900

    Auto stash before merge of "Navigator" and "origin/Navigator"

--- fe/src/components/Camera.js
+++ fe/src/components/Camera.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../css/Camera.css';
 import { useState } from 'react';
 import Webcam from "react-webcam";
+import { Link } from 'react-router-dom';
 // npm install react-webcam
 
 const Camera = () => {
@@ -15,6 +16,7 @@ const Camera = () => {
         e.preventDefault();
         fileInput.current.click();
     };
+
     const handleImageSelect = (e) => {
         const selectedImage = e.target.files[0];
         if (selectedImage) {
@@ -26,7 +28,7 @@ const Camera = () => {
     //webcam 사진캡쳐 부분
     const captureImage = (e) => {
         e.preventDefault();
-        const imageSrc = webcamRef.current.getScreenshot();
+        const imageSrc = webcamRef.current.getScreenshot(); //형식 지정하게 할 수 있니?
         setCapturedImage(imageSrc);
     };
     const resetImage = () => {
@@ -40,9 +42,9 @@ const Camera = () => {
         setIsActive(!isActive);
     };
 
-
     return (
         <div className='camera_container'>
+            <Link to="/" className='GotoHome' />
             <div className='screen_container'>
                 {capturedImage ? (
                     <img
@@ -55,6 +57,8 @@ const Camera = () => {
                         ref={webcamRef}
                         className='screen'
                         screenshotFormat='image/jpeg'
+                    // minScreenshotWidth={1080}
+                    // minScreenshotHeight={720}
                     />
                 )}
                 <div className='food_name'>
