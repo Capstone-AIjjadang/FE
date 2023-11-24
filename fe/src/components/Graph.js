628967ddecb355811d8bcb03ffb5aba2d135d180
안형조 <dsa2907@naver.com>
2023-11-24 14:05:10 +0900

    [feat] 영양성분 그래프 구현
    <rechart 컴포넌트 설치>
    npm install recharts

--- fe/src/components/Graph.js
+++ fe/src/components/Graph.js
@@ -3,36 +3,25 @@ import Navigator from './Navigator';
 import style from '../css/My.module.css';
 import { MdOutlineAlarmOn, MdOutlineRecommend } from "react-icons/md";
 import { CircularProgress, CircularProgressWithLabel } from '@mui/material';
-import { Line } from 'react-chartjs-2';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
 
 const Graph = () => {
 
-    const data = {
-        labels: ['월', '화', '수', '목', '금', '토', '일'],
-        datasets: [
-          {
-            label: '데이터',
-            data: [500, 300, 700, 200, 600, 800, 400],
-            fill: false,
-            borderColor: 'rgb(75, 192, 192)',
-            tension: 0.1,
-          },
-        ],
-      };
-
-      const options = {
-        scales: {
-          y: {
-            beginAtZero: true,
-            max: 1000,
-          },
-        },
-      };
+    const data = [
+        { day: '월', value: 500 },
+        { day: '화', value: 300 },
+        { day: '수', value: 700 },
+        { day: '목', value: 200 },
+        { day: '금', value: 600 },
+        { day: '토', value: 800 },
+        { day: '일', value: 400 },
+    ];
     
-      const handleEditClick = () => {
+    const handleEditClick = () => {
         // 여기에 정보 수정 모달 또는 다른 수정 방법을 구현할 수 있습니다.
         alert('Edit button clicked!');
-      };
+    };
 
     return (
         <div className={style.container}>
@@ -47,6 +36,20 @@ const Graph = () => {
                     정보 수정
                 </button>
             </div>
+
+            <LineChart
+                width={600}
+                height={300}
+                data={data}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="day" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="value" stroke="#8884d8" activeDot={{ r: 8 }} />
+            </LineChart>
         </div>
     );
 };
