7ebfbcbef95b0bb21070e67a1bcfecaa2e35170c
안형조 <dsa2907@naver.com>
2023-11-24 17:26:52 +0900

    [design] 네비게이션 바 선택 시 색 변경
    01. 네비게이션 js, css 수정
    02. my 모듈 마진 줄임

--- fe/src/components/Navigator.js
+++ fe/src/components/Navigator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../css/Navigator.module.css';
 import { Link } from 'react-router-dom';
 import { BiHomeAlt2, BiCamera } from 'react-icons/bi';
@@ -9,22 +9,29 @@ import { GoThumbsup } from 'react-icons/go';
 //npm install react-icons
 
 const Navigator = () => {
+    const [activeNav, setActiveNav] = useState(1);  
+
     return (
         <div className={styles.container}>
             <div className={styles.menu_bar}>
-                <Link to="/" className={styles.links}>
+                <Link to="/" className={`${styles.links} ${activeNav === 1 ? styles.active : ''}`}
+                onClick={() => setActiveNav(1)}>
                     <BiHomeAlt2 size='30' /><br />홈
                 </Link>
-                <Link to="/camera" className={styles.links}>
+                <Link to="/camera" className={`${styles.links} ${activeNav === 2 ? styles.active : ''}`}
+                onClick={() => setActiveNav(2)}>
                     <BiCamera size='30' /><br />촬영
                 </Link>
-                <Link to="/recommend" className={styles.links}>
+                <Link to="/recommend" className={`${styles.links} ${activeNav === 3 ? styles.active : ''}`}
+                onClick={() => setActiveNav(3)}>
                     <GoThumbsup size='30' /><br />음식추천
                 </Link>
-                <Link to="/graph" className={styles.links}>
+                <Link to="/graph" className={`${styles.links} ${activeNav === 4 ? styles.active : ''}`}
+                onClick={() => setActiveNav(4)}>
                     <BsGraphUp size='30' /><br />영양성분
                 </Link>
-                <Link to="/my" className={styles.links}>
+                <Link to="/my" className={`${styles.links} ${activeNav === 5 ? styles.active : ''}`}
+                onClick={() => setActiveNav(5)}>
                     <CgProfile size='30' /><br />내 건강정보
                 </Link>
             </div>
