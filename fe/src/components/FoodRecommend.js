dff3e76bf7b203cd203d1bd60e493d225a5f64bd
안형조 <dsa2907@naver.com>
2023-12-07 00:03:59 +0900

    [feat] 음식추천 4가지 코드 진행중

--- fe/src/components/FoodRecommend.js
+++ fe/src/components/FoodRecommend.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import style from '../css/FoodRecommend.module.css';
 import { MdOutlineAlarmOn, MdOutlineRecommend } from "react-icons/md";
 import { CircularProgress, CircularProgressWithLabel } from '@mui/material';
 
 const FoodRecommend = () => {
+    const [userData, setUserData] = useState(null);
+    // get
+    const fetchData = async () => {
+        try {
+            // FastAPI 서버로 요청을 보냅니다.
+            const response = await axios.get('http://localhost:8000/recommended_food/');
+
+            // 서버로부터 받은 데이터를 state에 저장합니다.
+            setUserData(response.data);
+            console.log('Name:', response.data[0].name1);
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+        }
+    };
+
+    useEffect(() => {
+        // fetchData 함수를 호출하여 데이터를 가져옵니다.
+        fetchData();
+    }, []);
+
     return (
         <div className={style.container}>
             <div className={style.header}>
@@ -32,10 +53,10 @@ const FoodRecommend = () => {
                             권장 칼륨은 00g 입니다.<br />
                             추천드리는 음식은 아래와 같습니다.</div>
                         <ul className={style.Rec_foods}>
-                            <li>01. 추천음식1</li>
-                            <li>02. 추천음식2</li>
-                            <li>03. 추천음식3</li>
-                            <li>04. 추천음식4</li>
+                            <li>01. {userData && userData.length > 0 ? userData[0].name1 : "Loading..."} </li>
+                            <li>02. {userData && userData.length > 0 ? userData[0].name2 : "Loading..."}</li>
+                            <li>03. {userData && userData.length > 0 ? userData[0].name3 : "Loading..."}</li>
+                            <li>04. {userData && userData.length > 0 ? userData[0].name4 : "Loading..."}</li>
                         </ul>
                     </div>
                 </div>
