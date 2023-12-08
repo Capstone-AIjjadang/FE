78fad1da1f71341947f9c696d8aa782f25ebb9d2
cj <toong1203@naver.com>
2023-12-08 16:45:41 +0900

    텍스트 결과인식 로딩 넣음

--- fe/src/components/TextAnalysis.js
+++ fe/src/components/TextAnalysis.js
@@ -4,7 +4,9 @@ import { LinearProgress } from '@mui/material';
 import { result_ocrAi, imageonly, todaysumfood, recommendintake } from './Api';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import Loading from './Loading';
 const TextAnalysis = () => {
+    const [loading, setLoading] = React.useState(true);
     const [rating, setRating] = React.useState(0);
     const [text, setText] = React.useState();
     const history = useNavigate();
@@ -70,7 +72,19 @@ const TextAnalysis = () => {
         setIntake(response);
     };
     const A = intake?.data.response_data || [];
+    React.useEffect(() => {
+        // 5초 동안의 지연을 시뮬레이션합니다.
+        const timeoutId = setTimeout(() => {
+            setLoading(false);
+        }, 3000);
 
+        // 메모리 누수를 방지하기 위해 timeout을 정리합니다.
+        return () => clearTimeout(timeoutId);
+    }, []); // 컴포넌트가 마운트될 때 한 번만 실행됩니다.
+
+    if (loading) {
+        return <Loading />; // 로딩 중일 때 Loading 컴포넌트를 표시합니다.
+    }
     return (
         <div className={style.container}>
             <div className={style.header}>
@@ -82,54 +96,54 @@ const TextAnalysis = () => {
                 </div>
                 <div className={style.analysis_container}>
                     {Text[Text.length - 1] && <div className={style.analysis}>
-                    <div className={style.sec1}>
-  <li>
-    총 칼로리 <span>{Math.floor(Text[Text.length - 1].text_cal * 10) / 10} Kcal</span>
-    <LinearProgress
-      variant="determinate"
-      value={(Math.floor(Text[Text.length - 1].text_cal * 10) / 10) / (Math.floor((A.recommended_cal - T.Total_food_cal) * 10) / 10) * 100 < 0 ? 100 : (Math.floor(Text[Text.length - 1].text_cal * 10) / 10) / (Math.floor((A.recommended_cal - T.Total_food_cal) * 10) / 10) * 100}
-      style={{ width: '100px', height: '14px', borderRadius: '10px' }}
-    />
-  </li>
-  <br /><br /><br />
-  <li>
-    탄수화물<span>{Math.floor(Text[Text.length - 1].text_carbs * 10) / 10} g</span>
-    <LinearProgress
-      variant="determinate"
-      value={(Math.floor(Text[Text.length - 1].text_carbs * 10) / 10) / (Math.floor((A.recommended_carbs - T.Total_food_carbs) * 10) / 10) * 100 < 0 ? 100 : (Math.floor(Text[Text.length - 1].text_carbs * 10) / 10) / (Math.floor((A.recommended_carbs - T.Total_food_carbs) * 10) / 10) * 100}
-      style={{ width: '100px', height: '14px', borderRadius: '10px' }}
-    />
-  </li>
-</div>
-<div className={style.sec2}>
-  <li>
-    나트륨<span>{Math.floor(Text[Text.length - 1].text_nat * 10) / 10} mg</span>
-    <LinearProgress
-      variant="determinate"
-      value={(Math.floor(Text[Text.length - 1].text_nat * 10) / 10) / (Math.floor((A.recommended_nat - T.Total_food_nat) * 10) / 10) * 100 < 0 ? 100 : (Math.floor(Text[Text.length - 1].text_nat * 10) / 10) / (Math.floor((A.recommended_nat - T.Total_food_nat) * 10) / 10) * 100}
-      style={{ width: '100px', height: '14px', borderRadius: '10px' }}
-    />
-  </li>
-  <br /><br /><br />
-  <li>
-    단백질<span>{Math.floor(Text[Text.length - 1].text_protein * 10) / 10} g</span>
-    <LinearProgress
-      variant="determinate"
-      value={(Math.floor(Text[Text.length - 1].text_protein * 10) / 10) / (Math.floor((A.recommended_protein - T.Total_food_protein) * 10) / 10) * 100 < 0 ? 100 : (Math.floor(Text[Text.length - 1].text_protein * 10) / 10) / (Math.floor((A.recommended_protein - T.Total_food_protein) * 10) / 10) * 100}
-      style={{ width: '100px', height: '14px', borderRadius: '10px' }}
-    />
-  </li>
-</div>
-<div className={style.sec3}>
-  <li>
-    지방<span>{Math.floor(Text[Text.length - 1].text_fat * 10) / 10} g</span>
-    <LinearProgress
-      variant="determinate"
-      value={(Math.floor(Text[Text.length - 1].text_fat * 10) / 10) / (Math.floor((A.recommended_fat - T.Total_food_fat) * 10) / 10) * 100 < 0 ? 100 : (Math.floor(Text[Text.length - 1].text_fat * 10) / 10) / (Math.floor((A.recommended_fat - T.Total_food_fat) * 10) / 10) * 100}
-      style={{ width: '100px', height: '14px', borderRadius: '10px' }}
-    />
-  </li>
-</div>
+                        <div className={style.sec1}>
+                            <li>
+                                총 칼로리 <span>{Math.floor(Text[Text.length - 1].text_cal * 10) / 10} Kcal</span>
+                                <LinearProgress
+                                    variant="determinate"
+                                    value={(Math.floor(Text[Text.length - 1].text_cal * 10) / 10) / (Math.floor((A.recommended_cal - T.Total_food_cal) * 10) / 10) * 100 < 0 ? 100 : (Math.floor(Text[Text.length - 1].text_cal * 10) / 10) / (Math.floor((A.recommended_cal - T.Total_food_cal) * 10) / 10) * 100}
+                                    style={{ width: '100px', height: '14px', borderRadius: '10px' }}
+                                />
+                            </li>
+                            <br /><br /><br />
+                            <li>
+                                탄수화물<span>{Math.floor(Text[Text.length - 1].text_carbs * 10) / 10} g</span>
+                                <LinearProgress
+                                    variant="determinate"
+                                    value={(Math.floor(Text[Text.length - 1].text_carbs * 10) / 10) / (Math.floor((A.recommended_carbs - T.Total_food_carbs) * 10) / 10) * 100 < 0 ? 100 : (Math.floor(Text[Text.length - 1].text_carbs * 10) / 10) / (Math.floor((A.recommended_carbs - T.Total_food_carbs) * 10) / 10) * 100}
+                                    style={{ width: '100px', height: '14px', borderRadius: '10px' }}
+                                />
+                            </li>
+                        </div>
+                        <div className={style.sec2}>
+                            <li>
+                                나트륨<span>{Math.floor(Text[Text.length - 1].text_nat * 10) / 10} mg</span>
+                                <LinearProgress
+                                    variant="determinate"
+                                    value={(Math.floor(Text[Text.length - 1].text_nat * 10) / 10) / (Math.floor((A.recommended_nat - T.Total_food_nat) * 10) / 10) * 100 < 0 ? 100 : (Math.floor(Text[Text.length - 1].text_nat * 10) / 10) / (Math.floor((A.recommended_nat - T.Total_food_nat) * 10) / 10) * 100}
+                                    style={{ width: '100px', height: '14px', borderRadius: '10px' }}
+                                />
+                            </li>
+                            <br /><br /><br />
+                            <li>
+                                단백질<span>{Math.floor(Text[Text.length - 1].text_protein * 10) / 10} g</span>
+                                <LinearProgress
+                                    variant="determinate"
+                                    value={(Math.floor(Text[Text.length - 1].text_protein * 10) / 10) / (Math.floor((A.recommended_protein - T.Total_food_protein) * 10) / 10) * 100 < 0 ? 100 : (Math.floor(Text[Text.length - 1].text_protein * 10) / 10) / (Math.floor((A.recommended_protein - T.Total_food_protein) * 10) / 10) * 100}
+                                    style={{ width: '100px', height: '14px', borderRadius: '10px' }}
+                                />
+                            </li>
+                        </div>
+                        <div className={style.sec3}>
+                            <li>
+                                지방<span>{Math.floor(Text[Text.length - 1].text_fat * 10) / 10} g</span>
+                                <LinearProgress
+                                    variant="determinate"
+                                    value={(Math.floor(Text[Text.length - 1].text_fat * 10) / 10) / (Math.floor((A.recommended_fat - T.Total_food_fat) * 10) / 10) * 100 < 0 ? 100 : (Math.floor(Text[Text.length - 1].text_fat * 10) / 10) / (Math.floor((A.recommended_fat - T.Total_food_fat) * 10) / 10) * 100}
+                                    style={{ width: '100px', height: '14px', borderRadius: '10px' }}
+                                />
+                            </li>
+                        </div>
 
                     </div>}
                 </div>
